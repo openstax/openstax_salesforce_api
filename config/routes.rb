8f288814e0059333b1b95a35a7670d9ce96b070c
Michael Harrison <mwharrison@rice.edu>
2020-06-23 16:28:50 -0500

    Add versionist for API versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
+    api_version(
+        module: "V1",
+        path: {value: "v1"},
+        defaults: {format: :json}
+    ) do
+
       resources :schools
+      
     end
   end
 end
