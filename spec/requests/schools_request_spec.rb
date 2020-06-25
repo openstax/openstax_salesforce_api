51ee87951d0e32220da2689cf18ddc62d91a9141
Michael Harrison <mwharrison@rice.edu>
2020-06-25 11:23:34 -0500

    add test for single school endpoint

--- spec/requests/schools_request_spec.rb
+++ spec/requests/schools_request_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Schools", type: :request do
 
-  it "it returns a successful response" do
+  it "returns a successful response for all schools" do
     get "/api/v1/schools/"
     expect(response).to be_successful
+  end
+
+  it "returns one school" do
+    school = School.first
+    get "/api/v1/schools/#{school.id}"
+
+    expect(response).to be_successful
+    expect(response.body).to match(school.name)
   end
 
 end
