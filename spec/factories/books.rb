eb6be066f40253b844109720908bfc5edc300a60
edwoodward <ecw1@rice.edu>
2020-07-27 14:27:30 -0500

    Added Books model, update and tests

---
+++ spec/factories/books.rb
@@ -0,0 +1,10 @@
+require 'faker'
+
+FactoryBot.define do
+  factory :book, class: OpenStax::Salesforce::Remote::Book do
+    skip_create
+    id {Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3, min_numeric: 3)}
+    name {Faker::Book.title}
+    
+  end
+end
