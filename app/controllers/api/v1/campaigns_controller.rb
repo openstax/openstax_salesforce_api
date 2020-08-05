cd226ba89918f89236a0d37f992db32ff2cccc3f
edwoodward <ecw1@rice.edu>
2020-08-05 16:28:54 -0500

    Updated controller comments

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::CampaignsController < ApplicationController
 
-  # GET /books
+  # GET /campaigns
   def index
     @campaigns = Campaign.paginate(page: params[:page], per_page: 20)
     render json: @campaigns
   end
 
-  # GET /books/:id
+  # GET /campaigns/:id
   def show
     begin
       @campaign = Campaign.where(salesforce_id: params[:id])
