0388d9d8094c72b32c31c5ef786816ff8b388366
edwoodward <ecw1@rice.edu>
2020-08-10 09:46:27 -0500

    Added CampaignMembers API

---
+++ app/controllers/api/v1/campaign_members_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::CampaignMembersController < ApplicationController
+
+  # GET /campaign_members
+  def index
+    @campaign_members = CampaignMember.paginate(page: params[:page], per_page: 20)
+    render json: @campaign_members
+  end
+
+  # GET /campaign_members/:id
+  def show
+    begin
+      @campaign_member = CampaignMember.where(salesforce_id: params[:id])
+      render json: @campaign_member, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: {
+          error: e.to_s
+      }, status: :not_found
+    end
+  end
+end
