bd8b8f8f9ee29e22c6987d1435d5e3cc3c43c957
edwoodward <ecw1@rice.edu>
2020-08-18 15:33:38 -0500

    Class to push opportunities to Salesforce and controller code to handle create and update

--- app/controllers/api/v1/opportunities_controller.rb
+++ app/controllers/api/v1/opportunities_controller.rb
@@ -17,4 +17,46 @@ class Api::V1::OpportunitiesController < ApplicationController
       }, status: :not_found
     end
   end
+
+  def create
+    push_opportunity = PushOpportunityToSalesforce.new
+    success = push_opportunity.create_new_opportunity(opportunity_params)
+    if success
+      render json: { status: 'Opportunity creation status: Success' }
+    else
+      render json: { status: 'Opportunity creation status: Failure' }
+    end
+  end
+
+  def update
+    push_opportunity = PushOpportunityToSalesforce.new
+    success = push_opportunity.update_opportunity(opportunity_params)
+    if success
+      render json: { opportunity_id: opportunity_params['salesforce_id'], status: 'Opportunity update status: Success' }
+    else
+      render json: { opportunity_id: opportunity_params['salesforce_id'], status: 'Opportunity update status: Failure' }
+    end
+  end
+
+  private
+
+  def opportunity_params
+    params.require(:opportunity).permit([
+      :term_year,
+      :book_name,
+      :contact_id,
+      :new,
+      :close_date,
+      :stage_name,
+      :type,
+      :number_of_students,
+      :student_number_status,
+      :time_period,
+      :class_start_date,
+      :school_id,
+      :book_id,
+      :contact_id,
+      :lead_source
+  ])
+  end
 end
---
+++ lib/push_opportunity_to_salesforce.rb
@@ -0,0 +1,62 @@
+class PushOpportunityToSalesforce
+
+  def find_or_create_opportunity(opportunity_data)
+    if opportunity_data['salesforce_id'] == ''
+      create_new_opportunity(opportunity_data)
+    else
+      update_opportunity(opportunity_data)
+    end
+
+  end
+
+  def create_new_opportunity(opportunity_data)
+    opportunity = OpenStax::Salesforce::Remote::Opportunity.new(
+        term_year: opportunity_data['term_year'],
+        book_name: opportunity_data['book_name'],
+        contact_id: opportunity_data['contact_id'],
+        new: opportunity_data['new'],
+        close_date: opportunity_data['close_date'],
+        stage_name: 'Confirmed Adoption (1)',
+        type: opportunity_data['update_type'],
+        number_of_students: opportunity_data['number_of_students'],
+        student_number_status: 'Reported',
+        time_period: 'Year',
+        class_start_date: opportunity_data['class_start_date'],
+        school_id: opportunity_data['school_id'],
+        book_id: Book.find(name: opportunity_data['book_name']).salesforce_id,
+        lead_source: 'openstax-salesforce-api'
+    )
+    opportunity.save();
+
+    return true unless opportunity.errors.any?
+      Rails.logger.warn('Error creating opportunity in salesforce:' + opportunity.errors.inspect)
+      false
+    end
+  end
+
+  def update_opportunity(opportunity_data)
+    opportunity = OpenStax::Salesforce::Remote::Opportunity.find(opportunity_data['salesforce_id'])
+    opportunity.update(
+        term_year: opportunity_data['term_year'],
+        book_name: opportunity_data['book_name'],
+        contact_id: opportunity_data['contact_id'],
+        new: opportunity_data['new'],
+        close_date: opportunity_data['close_date'],
+        stage_name: 'Confirmed Adoption (1)',
+        type: opportunity_data['update_type'],
+        number_of_students: opportunity_data['number_of_students'],
+        student_number_status: 'Reported',
+        time_period: 'Year',
+        class_start_date: opportunity_data['class_start_date'],
+        school_id: opportunity_data['school_id'],
+        book_id: Book.find(name: opportunity_data['book_name']).salesforce_id,
+        lead_source: 'openstax-salesforce-api'
+    )
+
+    return true unless opportunity.errors.any?
+      Rails.logger.warn('Error updating opportunity in salesforce:' + opportunity.errors.inspect)
+      false
+    end
+
+  end
+end
