731d3e47276df06a2694b166d5f461d8fbb1011f
Michael Harrison <mwharrison@rice.edu>
2020-08-19 08:04:40 -0500

    Add campaigns to swagger

---
+++ spec/integration/v1/campaigns_spec.rb
@@ -0,0 +1,50 @@
+require "swagger_helper"
+
+RSpec.describe "api/v1/campaigns", type: :request do
+  path "/api/v1/campaigns" do
+    get "List all Campaigns" do
+      tags "Campaigns"
+      consumes "application/json"
+      parameter name: :campaign, in: :body, schema: {
+        type: :object,
+        properties: {
+          salesforce_id: { type: :string },
+          name: { type: :string },
+          is_active: { type: :boolean }
+        },
+        required: ["salesforce_id", "name"],
+      }
+      response "200", "campaigns retrieved" do
+        let(:campaign) { FactoryBot.create :campaign }
+        run_test!
+      end
+    end
+  end
+
+  path "/api/v1/campaigns/{id}" do
+
+    get "Return one campaign" do
+      tags "Campaigns"
+      consumes "application/json"
+      parameter name: :id, in: :path, type: :string
+
+      response "200", "campaign found" do
+        schema type: :object,
+               properties: {
+                 salesforce_id: { type: :string },
+                 name: { type: :string },
+                 is_active: { type: :boolean }
+               },
+               required: ["salesforce_id", "name"]
+
+        let(:id) { Campaign.create(name: 'Test Campaign', salesforce_id: 'AZ44335AQ').id }
+        run_test!
+      end
+
+      response '404', 'campaign not found' do
+        let(:id) { 25 }
+        run_test!
+      end
+    end
+  end
+end
