e5d1dd97c11874cb70b7bdf4bcbb8b09bcd7ba0b
Michael Harrison <mwharrison@rice.edu>
2020-09-14 17:03:47 -0500

    update campaign spec to use id

--- spec/requests/campaigns_request_spec.rb
+++ spec/requests/campaigns_request_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe "Campaigns", type: :request do
   end
 
   it "return one campaign" do
-    get '/api/v1/campaigns/' + @campaign.salesforce_id
+    get "/api/v1/campaigns/#{@campaign.id}"
     expect(JSON.parse(response.body).size).to be >= 1
     expect(response).to have_http_status(:success)
   end
