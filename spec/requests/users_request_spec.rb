9803b4f8605144a674fc93a20c6b64a3ed9a49f5
edwoodward <ecw1@rice.edu>
2020-12-02 07:40:53 -0600

    Small change to trigger travis

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe 'Users', type: :request do
   before(:all) do
     opportunity = FactoryBot.create :api_opportunity
     contact = FactoryBot.create(:api_contact, salesforce_id: '0037X00000cVyfKQAS')
     lead = FactoryBot.create :api_lead
   end
 
-  it "returns a failure response because of missing cookie" do
+  it 'returns a failure response because of missing cookie' do
     get '/api/v1/users'
     expect(response).to have_http_status(:bad_request)
   end
 
-  it "returns data for user in cookie" do
+  it 'returns data for user in cookie' do
     headers = set_cookie
     get '/api/v1/users', :headers => headers
     expect(JSON.parse(response.body).size).to be >= 1
