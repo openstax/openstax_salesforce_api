6069ffd19ead380701bf0ddb2b57fde4e08105ca
edwoodward <ecw1@rice.edu>
2020-12-02 07:46:26 -0600

    Small change to trigger Travis

--- spec/requests/schools_request_spec.rb
+++ spec/requests/schools_request_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "Schools", type: :request do
+RSpec.describe 'Schools', type: :request do
 
   before(:all) do
     @school = FactoryBot.create :api_school
   end
 
-  it "returns a successful response for all schools" do
-    get "/api/v1/schools/"
+  it 'returns a successful response for all schools' do
+    get '/api/v1/schools/'
     expect(response).to have_http_status(:success)
   end
 
-  it "returns a successful response for school by name" do
-    get "/api/v1/schools/?name=" + @school.name
+  it 'returns a successful response for school by name' do
+    get '/api/v1/schools/?name=' + @school.name
     expect(response).to have_http_status(:success)
   end
 
-  it "returns one school by id" do
+  it 'returns one school by id' do
     get "/api/v1/schools/#{@school.id}"
     expect(JSON.parse(response.body).size).to be >= 1
     expect(response).to have_http_status(:success)
