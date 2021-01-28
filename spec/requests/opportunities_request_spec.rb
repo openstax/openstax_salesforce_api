6a6c4920086991b5178a4e067b4b5196925712ba
edwoodward <ecw1@rice.edu>
2021-01-28 14:43:02 -0600

    Updated VCR tests

--- spec/requests/opportunities_request_spec.rb
+++ spec/requests/opportunities_request_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe "Opportunities", type: :request, vcr: VCR_OPTS do
     contact = create_contact
     @headers = set_cookie
     @token_header = create_token_header
+    opportunity = FactoryBot.create(:api_opportunity, {:salesforce_id=>'0062F00000BG056QAD',:contact_id=>'0032F00000cfZQhQAM', :school_id=>'0012F00000iPxe9QAC'})
     VCR.use_cassette('OpportunitiesController/sf_setup', VCR_OPTS) do
       @proxy = SalesforceProxy.new
       @proxy.setup_cassette
@@ -55,15 +56,15 @@ RSpec.describe "Opportunities", type: :request, vcr: VCR_OPTS do
     expect(response).to have_http_status(:success)
   end
 
-  # it 'create new opportunity with token' do
-  #   opportunity_data = create_new_opportunity_data
-  #   headers = create_token_header
-  #   headers["ACCEPT"] = "application/json"
-  #   post "/api/v1/opportunities", :params => opportunity_data, :headers => headers
-  #
-  #   expect(response.content_type).to eq("application/json; charset=utf-8")
-  #   expect(response).to have_http_status(:success)
-  # end
+  it 'create new opportunity with token' do
+    opportunity_data = create_new_opportunity_data
+    headers = create_token_header
+    headers["ACCEPT"] = "application/json"
+    post "/api/v1/opportunities", :params => opportunity_data, :headers => headers
+
+    expect(response.content_type).to eq("application/json; charset=utf-8")
+    expect(response).to have_http_status(:success)
+  end
 
   it 'update opportunity' do
     @proxy = SalesforceProxy.new
@@ -77,29 +78,29 @@ RSpec.describe "Opportunities", type: :request, vcr: VCR_OPTS do
     expect(response).to have_http_status(:success)
   end
 
-  # it 'update opportunity with token' do
-  #   @proxy = SalesforceProxy.new
-  #   @proxy.setup_cassette
-  #   opportunity_data = create_update_opportunity_data
-  #   headers = create_token_header
-  #   headers["ACCEPT"] = "application/json"
-  #   put "/api/v1/opportunities/" + @opportunity.id.to_s, :params => opportunity_data, :headers => headers
-  #
-  #   expect(response.content_type).to eq("application/json; charset=utf-8")
-  #   expect(response).to have_http_status(:success)
-  # end
+  it 'update opportunity with token' do
+    @proxy = SalesforceProxy.new
+    @proxy.setup_cassette
+    opportunity_data = create_update_opportunity_data
+    headers = create_token_header
+    headers["ACCEPT"] = "application/json"
+    put "/api/v1/opportunities/" + @opportunity.id.to_s, :params => opportunity_data, :headers => headers
+
+    expect(response.content_type).to eq("application/json; charset=utf-8")
+    expect(response).to have_http_status(:success)
+  end
 
   def create_new_opportunity_data
     opportunity_data = { opportunity: {
       'salesforce_id': '',
       'term_year':'2019 - 20 Spring',
-      'book_name':'Behavior',
-      'contact_id':'0037X00000cVyfIQAS',
+      #'book_name':'Behavior',
+      'contact_id':'0032F00000cfZQhQAM',
       'new':true,
       'close_date':'2020-09-02',
       'number_of_students': 123,
       'class_start_date':'2020-09-02',
-      'school_id':'001U0000007gj6EIAQ',
+      'school_id':'0012F00000iPxe9QAC',
       'book_id':'zA9gH44q',
       'name': 'OS Salesforce API',
       'stage_name': 'Confirm Adoption Won'
@@ -107,23 +108,22 @@ RSpec.describe "Opportunities", type: :request, vcr: VCR_OPTS do
     }
   end
 
-    def create_update_opportunity_data
-
-        opportunity_data = { opportunity: {
+  def create_update_opportunity_data
+      opportunity_data = { opportunity: {
           'id':'',
-            'salesforce_id':'0067X000007eQMeQAM',
-            'term_year':'2019 - 20 Spring',
-            'book_name':'Behavior',
-            'contact_id':'0037X00000cVyfIQAS',
-            'new':true,
-            'close_date':'2020-09-02',
-            'number_of_students': 123,
-            'class_start_date':'2020-09-02',
-            'school_id':'001U0000007gj6EIAQ',
-            'book_id':'zA9gH44q',
-            'name': 'OS Salesforce API',
-            'stage_name': 'Confirm Adoption Won'
+          'salesforce_id':'0062F00000BG056QAD',
+          'term_year':'2019 - 20 Spring',
+          'book_name':'Behavior',
+          'contact_id':'0032F00000cfZQhQAM',
+          'new':true,
+          'close_date':'2020-09-02',
+          'number_of_students': 123,
+          'class_start_date':'2020-09-02',
+          'school_id':'0012F00000iPxe9QAC',
+          'book_id':'zA9gH44q',
+          'name': 'OS Salesforce API',
+          'stage_name': 'Confirm Adoption Won'
         }
-        }
-    end
+      }
+  end
 end
