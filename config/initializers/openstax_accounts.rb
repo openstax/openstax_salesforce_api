426263e356d1966d97c1e9b63f0ecdd5e9f13354
edwoodward <ecw1@rice.edu>
2021-01-29 15:38:28 -0600

    merge with Github changes

--- config/initializers/openstax_accounts.rb
+++ config/initializers/openstax_accounts.rb
@@ -1,6 +1,8 @@
 OpenStax::Accounts.configure do |config|
-  config.openstax_application_id = '1e0796d11bd4d92339c6a2f41c314440e90a7df91f302bbf332914c823c2d3d8'
-  config.openstax_application_secret = '0a556c378cb2615408b3612cf1376ed378dc562a97cc02e6173e66f4dcb41f7e'
-  config.openstax_accounts_url = 'https://accounts-dev.openstax.org'
-  config.enable_stubbing = false
+  oauth_secrets = Rails.application.secrets.oauth
+
+  config.openstax_application_id = oauth_secrets[:openstax_application_id]
+  config.openstax_application_secret = oauth_secrets[:openstax_application_secret]
+  config.openstax_accounts_url = oauth_secrets[:openstax_accounts_url]
+  config.enable_stubbing = oauth_secrets[:enable_stubbing]
 end
