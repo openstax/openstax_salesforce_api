966c2ec757b8236c5e9e6f210a190614553c7823
Michael Harrison <mwharrison@rice.edu>
2021-02-24 13:47:41 -0600

    move contact lists as model method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,39 +10,12 @@ class Api::V1::UsersController < ApplicationController
     # get leads
     lead = Lead.where(os_accounts_id: sso_cookie.dig('sub', 'id'))
 
-    # fetch local pardot subscriptions, if they exist, otherwise get them from Pardot
-    user_email_lists = []
-
-    if Subscription.exists?(contact: contact)
-      Subscription.where(contact: contact).each do |subscription|
-        user_email_lists.push({ id: subscription.list.pardot_id, title: subscription.list.title, description: subscription.list.description })
-      end
-
-    else
-      #TODO: move this somewhere shared, so it can also be used in update rake task
-      @client = Pardot::Client.client
-      @prospect = @client.prospects.read_by_fid(contact.salesforce_id)
-
-      @prospect['lists']['list_subscription'].each do |subscription|
-        next unless subscription['list']['is_public'] == 'true'
-
-        user_email_lists.push({ id: subscription['list']['id'], title: subscription['list']['name'], description: subscription['list']['description'] })
-
-        list = List.where(pardot_id: subscription['list']['id']).first_or_create do |plist|
-          plist.title = subscription['list']['title']
-          plist.description = subscription['list']['description']
-        end
-
-        Subscription.create list: list, contact: contact
-
-      end
-    end
 
     render json: {
       opportunity: opportunity,
       contact: contact,
       lead: lead,
-      subscriptions: user_email_lists
+      subscriptions: contact.list_subscriptions
     }
   end
 
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,33 @@
+require 'pardot/pardot'
+
 class Contact < ApplicationRecord
   validates :salesforce_id, presence: true, uniqueness: true
   has_many :subscriptions
   has_many :lists, through: :subscriptions
+
+  def list_subscriptions
+    subscriptions = []
+    if Subscription.exists?(contact: self)
+      Subscription.where(contact: self).each do |subscription|
+        subscriptions.push({ id: subscription.list.pardot_id, title: subscription.list.title, description: subscription.list.description })
+      end
+    else
+      @client = Pardot::Client.client
+      @prospect = @client.prospects.read_by_fid(self.salesforce_id)
+
+      @prospect['lists']['list_subscription'].each do |subscription|
+        next unless subscription['list']['is_public'] == 'true'
+
+        subscriptions.push({ id: subscription['list']['id'], title: subscription['list']['name'], description: subscription['list']['description'] })
+
+        list = List.where(pardot_id: subscription['list']['id']).first_or_create do |plist|
+          plist.title = subscription['list']['title']
+          plist.description = subscription['list']['description']
+        end
+
+        Subscription.create list: list, contact: self
+      end
+    end
+    subscriptions
+  end
 end
