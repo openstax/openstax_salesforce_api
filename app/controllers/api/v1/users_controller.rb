dc57ed2b6383c247c3e977113e7e677bb6fefeee
Michael Harrison <mwharrison@rice.edu>
2021-02-25 15:46:02 -0600

    change where clauses to be plural

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,16 +2,16 @@ class Api::V1::UsersController < ApplicationController
   def index
     sso_cookie = cookie_data
     return return_bad_request('User') if sso_cookie.blank?
-    opportunity = Opportunity.where(os_accounts_id: sso_cookie.dig('sub', 'id'))
+
+    opportunities = Opportunity.where(os_accounts_id: sso_cookie.dig('sub', 'id'))
     contact = Contact.find_by(salesforce_id: sso_cookie.dig('sub', 'salesforce_contact_id'))
-    lead = Lead.where(os_accounts_id: sso_cookie.dig('sub', 'id'))
+    leads = Lead.where(os_accounts_id: sso_cookie.dig('sub', 'id'))
     list_subscriptions = contact.list_subscriptions unless contact.nil?
 
-
     render json: {
-      opportunity: opportunity,
+      opportunity: opportunities,
       contact: contact,
-      lead: lead,
+      lead: leads,
       subscriptions: list_subscriptions
     }
   end
