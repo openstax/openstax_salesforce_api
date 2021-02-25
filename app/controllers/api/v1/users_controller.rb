a847af2137ddd38df8b165e8f456a87184f1701d
Michael Harrison <mwharrison@rice.edu>
2021-02-25 15:44:24 -0600

    use find_by to return 1 contact in user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
     sso_cookie = cookie_data
     return return_bad_request('User') if sso_cookie.blank?
     opportunity = Opportunity.where(os_accounts_id: sso_cookie.dig('sub', 'id'))
-    contact = Contact.where(salesforce_id: sso_cookie.dig('sub', 'salesforce_contact_id'))
+    contact = Contact.find_by(salesforce_id: sso_cookie.dig('sub', 'salesforce_contact_id'))
     lead = Lead.where(os_accounts_id: sso_cookie.dig('sub', 'id'))
     list_subscriptions = contact.list_subscriptions unless contact.nil?
 
