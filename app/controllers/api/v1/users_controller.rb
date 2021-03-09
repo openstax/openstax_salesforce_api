87e559f75a63a249d81e0b72bbbce859dec90a6a
Michael Harrison <mwharrison@rice.edu>
2021-03-09 08:39:26 -0600

    check sso cookie in before action on user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,6 @@ class Api::V1::UsersController < Api::V1::BaseController
   before_action -> { verify_sso_cookie }
 
   def index
-    sso_cookie = cookie_data
-    return return_bad_request('User') if sso_cookie.blank?
-
     opportunities = Opportunity.where(os_accounts_id: sso_cookie_field('id'))
     contact = Contact.find_by(salesforce_id: sso_cookie_field('salesforce_contact_id'))
     leads = Lead.where(os_accounts_id: sso_cookie_field('id'))
@@ -17,5 +14,4 @@ class Api::V1::UsersController < Api::V1::BaseController
       subscriptions: list_subscriptions
     }
   end
-
 end
