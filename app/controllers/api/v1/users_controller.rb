b210efc8fd4aac1729632c0f7d4a21dd0fd2bdd7
Michael Harrison <mwharrison@rice.edu>
2021-03-17 13:07:12 -0500

    use current contact in the user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,12 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def index
     opportunities = Opportunity.where(os_accounts_id: sso_cookie_field('id'))
-    contact = Contact.find_by(salesforce_id: sso_cookie_field('salesforce_contact_id'))
     leads = Lead.where(os_accounts_id: sso_cookie_field('id'))
-    list_subscriptions = contact.list_subscriptions unless contact.nil?
+    list_subscriptions = current_contact&.list_subscriptions
 
     render json: {
       opportunity: opportunities,
-      contact: contact,
+      contact: current_contact,
       lead: leads,
       subscriptions: list_subscriptions
     }
