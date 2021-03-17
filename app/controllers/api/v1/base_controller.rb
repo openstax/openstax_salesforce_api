004e86de14c8379a06e92aa3f133e4553fbda1cd
Michael Harrison <mwharrison@rice.edu>
2021-03-17 14:41:57 -0500

    pass error message to bad request

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,16 +3,12 @@ class Api::V1::BaseController < ApplicationController
 
   include RescueFromUnlessLocal
 
-  rescue_from_unless_local StandardError, send_to_sentry: true do |ex|
-    render json: { error: ex.message }, status: 500
-  end
-
   rescue_from_unless_local CannotFindUserContact, send_to_sentry: false do |ex|
     render json: { error: 'Cannot find Salesforce User' }, status: :not_found
   end
 
   rescue_from_unless_local BadRequest, send_to_sentry: true do |ex|
-    render json: { error: 'Bad request' }, status: :bad_request
+    render json: { error: ex.message }, status: :bad_request
   end
 
   rescue_from_unless_local NotAuthorized do |ex|
