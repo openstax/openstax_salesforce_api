e59193cf87f9c2b366d939b6f6b7f977ad1e0e98
edwoodward <ecw1@rice.edu>
2021-03-17 14:39:41 -0500

    Swagger test for UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,20 +2,19 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     sso_cookie = cookie_data
-    if sso_cookie.blank?
-      return return_bad_request('User')
-    end
-    #get opportunity
+    render json: { status: 'bad request' }.to_json, status: :bad_request and return if sso_cookie.blank?
+
     opportunity = Opportunity.where(os_accounts_id: sso_cookie.dig('sub', 'id'))
-    # get contact
-    contact = Contact.where(salesforce_id: sso_cookie.dig('sub', 'salesforce_contact_id'))
-    # get leads
+    cookie_uuid = sso_cookie.dig('sub', 'uuid')
+    results = JSON.parse(OpenStax::Accounts::Api.search_accounts("uuid:#{cookie_uuid}", options = {}).body)
+    contact_id = results['items'][0]['salesforce_contact_id'] unless results['items'].blank?
+    contact = Contact.where(salesforce_id: contact_id)
     lead = Lead.where(os_accounts_id: sso_cookie.dig('sub', 'id'))
 
     render json: {
-      :opportunity => opportunity,
-      :contact => contact,
-      :lead => lead
+      opportunity: opportunity,
+      contact: contact,
+      lead: lead
     }
   end
 end
---
+++ spec/integration/v1/users_spec.rb
@@ -0,0 +1,37 @@
+require 'swagger_helper'
+require 'rails_helper'
+require 'spec_helper'
+require 'vcr_helper'
+
+RSpec.describe 'api/v1/users', type: :request, vcr: VCR_OPTS do
+
+  before(:all) do
+    @contact = create_contact
+    @lead = FactoryBot.create :api_lead
+    @opportunity = FactoryBot.create :api_lead
+    VCR.use_cassette('UsersController/sf_setup', VCR_OPTS) do
+      @proxy = SalesforceProxy.new
+      @proxy.setup_cassette
+    end
+  end
+
+  path '/api/v1/users' do
+    get 'returns data for user in cookie' do
+      tags 'Users'
+      consumes 'application/json'
+      security [apiToken: []]
+
+      response '200', 'user data retrieved' do
+        let(:HTTP_COOKIE) { oxa_cookie }
+
+        run_test!
+      end
+
+      response '400', 'no cookie' do
+        let(:HTTP_COOKIE) { }
+
+        run_test!
+      end
+    end
+  end
+end
