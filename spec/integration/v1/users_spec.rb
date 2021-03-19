34a7cb74a7bcfdf7518ceac80aaa2fada2d3f21b
edwoodward <ecw1@rice.edu>
2021-03-19 07:53:31 -0500

    Tests now working with mock Accounts data

--- spec/integration/v1/users_spec.rb
+++ spec/integration/v1/users_spec.rb
@@ -1,36 +1,45 @@
-require 'swagger_helper'
 require 'rails_helper'
-require 'spec_helper'
 require 'vcr_helper'
+require 'swagger_helper'
 
-RSpec.describe 'api/v1/users', type: :request, vcr: VCR_OPTS do
+RSpec.describe 'api/v1/users', type: :request do
 
   before(:all) do
-    @contact = create_contact
+    @opportunity = FactoryBot.create :api_opportunity
     @lead = FactoryBot.create :api_lead
-    @opportunity = FactoryBot.create :api_lead
-    VCR.use_cassette('UsersController/users', VCR_OPTS) do
-      @proxy = SalesforceProxy.new
-      @proxy.setup_cassette
-    end
+    @contact = create_contact(salesforce_id: '0030v00000UlS9yAAF')
   end
 
   path '/api/v1/users' do
-    get 'returns data for user in cookie' do
+    get 'Get user info' do
       tags 'Users'
-      consumes 'application/json'
+      produces 'application/json'
       security [apiToken: []]
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          opportunity: { type: :array },
+          contact: { type: :array },
+          lead: { type: :array }
+        }
+      }
 
-      response '200', 'user data retrieved' do
+      response '200', 'user retrieved' do
+        before do
+          expect(OpenStax::Accounts::Api).to receive(:search_accounts).with('uuid:57bbe3d3-d630-4e9c-bc22-f86b701381a0', options = {}).and_return Hashie::Mash.new('body' => search_accounts_result)
+        end
         let(:HTTP_COOKIE) { oxa_cookie }
-
-        run_test!
+        run_test! do |response|
+          expect(JSON.parse(response.body).size).to be >= 1
+          expect(response).to have_http_status(:success)
+        end
       end
 
-      response '400', 'no cookie' do
-        let(:HTTP_COOKIE) { }
-
-        run_test!
+      response '400', 'invalid cookie' do
+        let(:HTTP_COOKIE) { 'invalid' }
+        run_test! do |response|
+          expect(response).to have_http_status(:bad_request)
+        end
       end
     end
   end
