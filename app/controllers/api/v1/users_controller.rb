7b348d06c8faeae9b46c3b699ff9df059058fc10
Michael Harrison <mwharrison@rice.edu>
2021-03-31 13:40:46 -0500

    move list_subscription array building to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,16 @@ class Api::V1::UsersController < Api::V1::BaseController
   def index
     opportunities = Opportunity.where(os_accounts_id: sso_cookie_field('id'))
     leads = Lead.where(os_accounts_id: sso_cookie_field('id'))
-    list_subscriptions = current_contact&.list_subscriptions
+
+    list_subscriptions = []
+    # Add all the lists to the subscriptions array, we update subscribed to true below if the user is subscribed.
+    # This reduces calls to the API by providing all available lists in the users API.
+    List.all.each do |list|
+      list_subscriptions.push({ id: list.pardot_id,
+                                title: list.title,
+                                description: list.description,
+                                subscribed: current_contact&.subscriptions.any? { |subscription| subscription.list == list } })
+    end
 
     render json: {
       opportunity: opportunities,
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,17 +2,4 @@ class Contact < ApplicationRecord
   validates :salesforce_id, presence: true, uniqueness: true
   has_many :subscriptions
   has_many :lists, through: :subscriptions
-
-  def list_subscriptions
-    @list_subscriptions = []
-    # Add all the lists to the subscriptions array, we update subscribed to true below if the user is subscribed.
-    # This reduces calls to the API by providing all available lists in the users API.
-    List.all.each do |list|
-      @list_subscriptions.push({ id: list.pardot_id,
-                                 title: list.title,
-                                 description: list.description,
-                                 subscribed: subscriptions.any? { |subscription| subscription.list == list } })
-    end
-    @list_subscriptions
-  end
 end
