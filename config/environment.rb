0dea92c24137811b067efad9ac6a687afec2ea6c
Michael Harrison <mwharrison@rice.edu>
2021-03-31 13:45:51 -0500

    move sidekiq require to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require 'pardot/patches'
 require 'rescue_from_unless_local'
 require 'errors'
 require 'salesforce/client'
+require 'sidekiq/web'
 
 # Initialize the Rails application.
 Rails.application.initialize!
