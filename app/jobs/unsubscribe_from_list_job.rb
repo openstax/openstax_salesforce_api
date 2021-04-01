f2f0625a59f4ae34ac64e420cf9ae211aeda1262
Michael Harrison <mwharrison@rice.edu>
2021-04-01 10:18:57 -0500

    blow up if subscription destroy doesn't work

--- app/jobs/unsubscribe_from_list_job.rb
+++ app/jobs/unsubscribe_from_list_job.rb
@@ -6,8 +6,6 @@ class UnsubscribeFromListJob < ApplicationJob
     return unless prospect_id = Pardot.salesforce_to_prospect(subscription.contact.salesforce_id)
 
     Pardot.client.list_memberships.delete(subscription.list.pardot_id, prospect_id)
-
-    # destroy the subscription record but if that fails, recreate it in Pardot
-    Pardot.client.list_memberships.create(subscription.list.pardot_id, prospect_id) unless subscription.destroy
+    subscription.destroy!
   end
 end
