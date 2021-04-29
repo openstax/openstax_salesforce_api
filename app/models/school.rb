8e8cade9c7f684f4b7b236f9a1e2e23e40ca15e2
edwoodward <ecw1@rice.edu>
2021-04-29 11:33:06 -0500

    made school name case insensitive

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,6 +3,6 @@ class School < ApplicationRecord
 
   def self.search(name, limit)
     limit = 150 if limit.nil?
-    where('name LIKE ?', "%#{name}%").limit(limit)
+    where('lower(name) LIKE ?', "%#{name}%".downcase).limit(limit)
   end
 end
