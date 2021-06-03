74b9462a6a8bd5510178aed7d9c3bad46c452609
Michael Harrison <mwharrison@rice.edu>
2021-06-03 13:31:07 -0500

    delete by relation id

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -28,17 +28,15 @@ class Api::V1::ContactsController < Api::V1::BaseController
     relation.save
 
     if relation.errors.any?
-      Rails.logger.warn('Error creating opportunity in salesforce:' + relation.errors.inspect)
+      Rails.logger.warn('Error creating school/contact relation in salesforce:' + relation.errors.inspect)
     end
 
-    #PushContactSchoolToSalesforceJob(@contact, @school)
   end
 
-  # DELETE /contacts/:contact_id/:school_id
+  # DELETE /contacts/:relation_id
   def remove_school
-    OpenStax::Salesforce::Remote::AccountContactRelation.destory_by(
-      contact_id: params[:contact_id],
-      school_id: params[:school_id]
+    OpenStax::Salesforce::Remote::AccountContactRelation.destory(
+      id: params[:relation_id]
     )
   end
 end
