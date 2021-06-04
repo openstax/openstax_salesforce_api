75e0403fef12e0b4aef850a97f1245261951f047
Michael Harrison <mwharrison@rice.edu>
2021-06-04 11:43:50 -0500

    account for multiple schools

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   def index
     opportunities = Opportunity.where(os_accounts_id: sso_cookie_field('id'))
     leads = Lead.where(os_accounts_id: sso_cookie_field('id'))
-    schools = AccountContactRelation.find_by(contact_id: current_contact&.id)
+    schools = AccountContactRelation.where(contact_id: current_contact&.salesforce_id)
 
     render json: {
       opportunity: opportunities,
