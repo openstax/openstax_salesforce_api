49084acf36b23340569d12b675266cd617aa3908
Michael Harrison <mwharrison@rice.edu>
2021-07-26 15:54:17 -0500

    send dead jobs to sentry for monitoring

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,8 +2,13 @@ redis_config = Rails.application.secrets.redis
 
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
+
+  config.death_handlers << ->(job, ex) do
+	  Sentry.capture_message "Uh oh, #{job['class']} #{job["jid"]} just died with error #{ex.message}."
+  end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/12" }
 end
+
