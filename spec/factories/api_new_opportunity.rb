d2337fa4d8ebfa993e7f9d3d346689718369292b
edwoodward <ecw1@rice.edu>
2021-08-30 15:20:33 -0500

    Can create and update Opportunities

--- spec/factories/api_new_opportunity.rb
+++ spec/factories/api_new_opportunity.rb
@@ -1,22 +1,22 @@
 require 'faker'
-require 'time'
 
 FactoryBot.define do
-  factory :api_opportunity, class: Opportunity do
-    salesforce_id { '0064C00000DtyZIQAZ' }
+  factory :api_new_opportunity, class: Opportunity do
     term_year { [ '2019 - 20 Spring', '2019 - 20 Fall', '2018 - 19 Fall', '2018 - 19 Spring'].sample }
-    book_name { 'Managerial Accounting' }
-    contact_id { '0034C00000Toa9PQAR' }
-    new { false }
-    close_date { Date.today - 2 }
+    book_name { 'Prealgebra' }
+    contact_id { '0034C00000T6UZLQA3' }
+    new { [true, false].sample }
+    close_date { Date.today }
     stage_name { 'Confirm Adoption Won' }
     update_type { 'Renewal - Verified' }
     number_of_students { Faker::Number.between(from: 1, to: 200) }
     student_number_status { 'Reported' }
     time_period { 'Year' }
     class_start_date { Date.today }
-    school_id { '0014C00000aB0swQAC' }
-    book_id { 'a0Z4C000002JXWSUA4' }
+    school_id { '0014C00000ZiK4nQAF' }
+    book_id { '' }
     lead_source { 'Web' }
+    os_accounts_id { 1 }
+    name { Faker::Superhero.name }
   end
 end
--- spec/jobs/push_opportunity_to_salesforce_job_spec.rb
+++ spec/jobs/push_opportunity_to_salesforce_job_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 require 'vcr_helper'
 Sidekiq::Testing.inline!
 
-RSpec.describe PushOpportunityToSalesforceJob, type: :job do
+RSpec.describe PushOpportunityToSalesforceJob, type: :job, vcr: VCR_OPTS do
   before(:all) do
     @opportunity = FactoryBot.create :api_opportunity
+    @new_opportunity = FactoryBot.create :api_new_opportunity
+    # create books for test
+    FactoryBot.create(:api_book, { name: 'Managerial Accounting', salesforce_id: 'a0Z4C000002JXWSUA4' })
+    FactoryBot.create(:api_book, { name: 'Prealgebra', salesforce_id: 'a0Z4C000002JRFOUA4' })
+    VCR.use_cassette('PushOpportunityToSalesforceJob/sf_setup', VCR_OPTS) do
+      @proxy = SalesforceProxy.new
+      @proxy.setup_cassette
+    end
   end
 
   before(:each) do
@@ -14,8 +22,14 @@ RSpec.describe PushOpportunityToSalesforceJob, type: :job do
   it { is_expected.to be_processed_in :default }
   it { is_expected.to be_retryable true }
 
-  it 'pushes an opportunity' do
+  it 'pushes an existing opportunity' do
     PushOpportunityToSalesforceJob.new.perform(@opportunity)
+    expect(@opportunity.salesforce_id).to_not be_nil
+    expect(@opportunity.lead_source).to eq('Web')
+  end
+
+  it 'pushes a new opportunity' do
+    PushOpportunityToSalesforceJob.new.perform(@new_opportunity)
     expect(@opportunity.salesforce_id).to_not be_nil
     expect(@opportunity.lead_source).to eq('Web')
   end
