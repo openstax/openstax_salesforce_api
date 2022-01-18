cd58280807c7ab723a065b9e016f13abd370163b
Michael Harrison <mwharrison@rice.edu>
2022-01-18 17:20:21 -0600

    trying a different way

--- config/initializers/openstax_accounts.rb
+++ config/initializers/openstax_accounts.rb
@@ -1,8 +1,6 @@
 OpenStax::Accounts.configure do |config|
-  secrets = Rails.application.secrets.accounts
-
-  config.openstax_application_id = secrets[:openstax_application_id]
-  config.openstax_application_secret = secrets[:openstax_application_secret]
-  config.openstax_accounts_url = secrets[:openstax_accounts_url]
-  config.enable_stubbing = ActiveAttr::Typecasting::BooleanTypecaster.new.call(secrets[:enable_stubbing])
+  config.openstax_application_id = Rails.application.secrets.accounts[:openstax_application_id]
+  config.openstax_application_secret = Rails.application.secrets.accounts[:openstax_application_secret]
+  config.openstax_accounts_url = Rails.application.secrets.accounts[:openstax_accounts_url]
+  config.enable_stubbing = ActiveAttr::Typecasting::BooleanTypecaster.new.call(Rails.application.secrets.accounts[:enable_stubbing])
 end unless secrets[:openstax_accounts_url].nil?
