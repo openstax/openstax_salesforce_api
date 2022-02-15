f441c6c3b18d5db56076403884bb6072cd0bd170
Michael Harrison <mwharrison@rice.edu>
2022-02-14 23:02:35 -0600

    remove old account migrations

--- db/migrate/20210111145459_add_salesforce_contact_id_to_accounts_accounts.openstax_accounts.rb
+++
@@ -1,7 +0,0 @@
-# This migration comes from openstax_accounts (originally 6)
-class AddSalesforceContactIdToAccountsAccounts < ActiveRecord::Migration[4.2]
-  def change
-    add_column :openstax_accounts_accounts, :salesforce_contact_id, :string
-    add_index :openstax_accounts_accounts, :salesforce_contact_id
-  end
-end
