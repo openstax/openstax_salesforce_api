2030beadf48f6299409a202e272b8694aa541971
Michael Harrison <mwharrison@rice.edu>
2022-02-14 22:46:02 -0600

    remove old ported account migrations

--- db/migrate/20210111145470_add_school_location_to_openstax_accounts_accounts.openstax_accounts.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from openstax_accounts (originally 17)
-class AddSchoolLocationToOpenStaxAccountsAccounts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :openstax_accounts_accounts, :school_location, :integer, default: 0, null: false
-  end
-end
