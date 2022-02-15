ac838c47ceddbedc013722e19cbfdf864ea379fa
Michael Harrison <mwharrison@rice.edu>
2022-02-14 21:55:18 -0600

    verify school exists before creating relation

--- app/models/account_contact_relation.rb
+++ app/models/account_contact_relation.rb
@@ -1,6 +1,17 @@
 class AccountContactRelation < ApplicationRecord
-  has_many :schools, through: :schools
-  has_many :contacts, through: :contacts
+  has_one :school
+  has_one :contact
+
+  before_validation :ensure_school_exists_in_api
 
   validates_uniqueness_of :contact_id, scope: :school_id
+
+  private
+
+  def ensure_school_exists_in_api
+    return if School.find_by(salesforce_id: school_id).exists?
+
+    sf_school = OpenStax::Salesforce::Remote::School.find_by!(school_id)
+    School.cache_school(sf_school)
+  end
 end
