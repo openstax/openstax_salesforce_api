f605c51aa201e559a87d3d3cfc665049452c716e
Michael Harrison <mwharrison@rice.edu>
2022-02-16 21:38:00 -0600

    taking python out of ruby

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,10 +8,10 @@ class Book < ApplicationRecord
   # expects an object of type
   # OpenStax::Salesforce::Remote::Book
   def self.cache_local(sf_book)
-    self.find_or_initialize_by(salesforce_id: sf_book.id)
-    self.name = sf_book.name
+    book = self.find_or_initialize_by(salesforce_id: sf_book.id)
+    book.name = sf_book.name
 
-    self.save if self.changed?
-    self
+    book.save if book.changed?
+    book
   end
 end
